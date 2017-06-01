c82edf3c9d438f84e32613010bd9ec82b91aebfa
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2017-06-01 10:32:01 +0100

    add something

--- app/uk/gov/hmrc/upLoadXMLToMongo/InterceptIdempotentFilter.scala
+++ app/uk/gov/hmrc/upLoadXMLToMongo/InterceptIdempotentFilter.scala
@@ -13,21 +13,28 @@ trait InterceptIdempotentFilter {
                                   (implicit bodyParser: BodyParser[T]): Action[T] =
     Action.async(bodyParser) { implicit request =>
 
+      val startTime = System.currentTimeMillis
+
       if (idempotent.contains(request.tags.getOrElse("ROUTE_VERB", ""))) {
         Logger.info("this request is an idempotent request")
-        isRequiringNRepudiation(request.headers)
+        addNRepudiationLogger(request.headers)
         Logger.info(s"the request headers are: ${request.headers.toString}")
         Logger.info(s"the request body is: ${request.body.toString}")
       }
       else {
         Logger.info("this request is not an idempotent request")
-        isRequiringNRepudiation(request.headers)
+        addNRepudiationLogger(request.headers)
       }
 
-      furtherAction(request)
+      val endTime = System.currentTimeMillis
+      val requestTime = endTime - startTime
+
+      Logger.info(s"${request.method} ${request.uri} took ${requestTime}ms")
+
+      furtherAction(request.headers.add("some-header"->"some-header"))
     }
 
-  def isRequiringNRepudiation(headers: Headers): Any = {
+  def addNRepudiationLogger(headers: Headers): Any = {
     if (headers.get("non-repudiation").contains("true")) Logger.info("this request is requiring non-repudiation")
     else Logger.info("this request is not requiring non-repudiation")
   }
